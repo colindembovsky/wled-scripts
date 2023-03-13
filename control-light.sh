e57aaa160fc6e6f2a756428d97c2fda0f54a05fe
colindembovsky <colindembovsky@gmail.com>
2023-03-13 09:15:58 -0500

    Stuffs

---
+++ control-light.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+# this script is used to control WLED light strip
+ip=${1:-'10.0.0.160'}
+command=$2
+ps=$3
+
+if [ "$command" = "on" ]; then
+    curl -X POST -H "Content-Type: application/json" -d '{"on":true}' http://$ip/json/state
+elif [ "$command" = "off" ]; then
+    curl -X POST -H "Content-Type: application/json" -d '{"on":false}' http://$ip/json/state
+elif [ "$command" = "ps" ]; then
+    curl -X POST -H "Content-Type: application/json" -d "{'on':true, 'ps':${ps}}" http://$ip/json
+fi
---
+++ light-universe.sh
@@ -0,0 +1,1 @@
+./control-light.sh "" ps 1
